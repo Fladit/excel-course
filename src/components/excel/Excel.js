666450d25002ef37d4d973064786831fac3faaec
Fladit <zatsarin77@gmail.com>
2020-10-18 20:10:41 +0300

    lesson 31

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,16 @@ export class Excel {
         this.$el = document.querySelector(selector);
         this.components = options.components || []
     }
+    
+    getRoot() {
+        const $root = document.createElement("div");
+        this.components.forEach(Component => {
+            const newComponent = new Component();
+            $root.insertAdjacentHTML("beforeend", newComponent.toHTML());
+        })
+        return $root;
+    }
+    render() {
+        this.$el.append(this.getRoot())
+    }
 }
