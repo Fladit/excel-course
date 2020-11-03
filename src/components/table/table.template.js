f9e0c6c077cc7778f67e82fc3a8f2650f9d7a692
Fladit <zatsarin77@gmail.com>
2020-11-03 22:28:03 +0300

    lesson-37, create table

---
+++ src/components/table/table.template.js
@@ -0,0 +1,45 @@
+const CODES = {
+    A: 65,
+    Z: 90,
+}
+
+
+function createRow(value, index = '' ) {
+    return (`
+<div class="row">
+    <div class="row-info">${index}</div>
+    <div class="row-data">${value}</div>
+</div>`)
+}
+
+function createColumn(value = '') {
+    return `<div class="column">${value}</div>`
+}
+
+function createCell(value = '') {
+    return `<div class="cell" contenteditable="">${value}</div>`
+}
+
+function toChar(_, ind) {
+    return String.fromCharCode(CODES.A + ind)
+}
+
+export default function createTable(rowsCount) {
+    const columnsCount = CODES.Z - CODES.A + 1;
+    const rows = []
+    const headerColumns = new Array(columnsCount)
+        .fill('')
+        .map(toChar)
+        .map(createColumn)
+        .join('')
+    rows.push(createRow(headerColumns))
+    for (let i = 1; i <= rowsCount; i++) {
+        const cells = new Array(columnsCount)
+            .fill('')
+            .map(createCell)
+            .join('')
+        rows.push(createRow(cells, i))
+    }
+    return rows.join('')
+}
+
